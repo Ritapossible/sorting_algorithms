34de8aaa1f7cefa5d46b22250887ba082a28f4e1
Ritapossible <egwuaturita95@gmail.com>
2023-11-20 05:08:03 -0800

    Corrected to betty standard

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -38,7 +38,8 @@ void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 }
 
 /**
- * merge_sort_recursive - This implements the merge sort algorithm through recursion.
+ * merge_sort_recursive - This implements the merge sort
+ * algorithm through recursion.
  * @subarr: A subarray of an array of integers to sort.
  * @buff: A buffer to store the sorted result.
  * @front: The front index of the subarray.
